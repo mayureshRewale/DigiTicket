ee2fe90cd9d540a8efa51edd915b039b7489f797
mayureshRewale <mayuresh.rewale@neosoftmail.com>
2023-10-21 09:01:01 +0530

    Added test API URL for master Service

--- DTMasterService/src/main/java/np/dt/dtmasterservice/controller/DTMasterController.java
+++ DTMasterService/src/main/java/np/dt/dtmasterservice/controller/DTMasterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/master")
 public class DTMasterController {
 
-    @GetMapping("/master")
+    @GetMapping("/test")
     public String masterTest(){
         return "DD";
     }
