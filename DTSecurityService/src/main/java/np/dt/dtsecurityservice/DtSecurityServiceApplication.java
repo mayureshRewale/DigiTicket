d02d451dafaec1cc30a0adc004309c236bc24a20
mayureshRewale <mayuresh.rewale@neosoftmail.com>
2023-10-21 09:06:40 +0530

    Added Security Service

---
+++ DTSecurityService/src/main/java/np/dt/dtsecurityservice/DtSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package np.dt.dtsecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtSecurityServiceApplication.class, args);
+	}
+
+}
---
+++ DTSecurityService/src/test/java/np/dt/dtsecurityservice/DtSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package np.dt.dtsecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
